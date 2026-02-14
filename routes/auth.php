2e19955effc98fc1f01f06ea941ca54dc61f5be7
Exau Mazaba <exaucemazaba6@gmail.com>
2026-02-15 00:32:20 +0100

    Final cleanup: disabled public registration

--- routes/auth.php
+++ routes/auth.php
@@ -12,10 +12,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Les routes 'register' ont été supprimées pour désactiver l'inscription publique
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
